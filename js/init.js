ce2317e470c75ed687edf73132b42d11f0fce97c
Adrienne Beaudry <adrienne.beaudry@gmail.com>
2017-03-23 23:07:44 +0100

    demo site IEP version 1

---
+++ js/init.js
@@ -0,0 +1,12 @@
+(function($){
+  $(function(){
+
+    $('.carousel').carousel();
+      $('.button-collapse').sideNav();
+      
+
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+
